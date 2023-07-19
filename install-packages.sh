2622285cc1367b996ea0c1afb98817ac07ed706c
pairnon <janpairnon@gmail.com>
2023-07-19 00:11:45 -0700

    add thunar

--- install-packages.sh
+++ install-packages.sh
@@ -4,6 +4,6 @@ sudo pacman -Syy
 
 sudo pacman -Syu
 
-sudo pacman -S i3status htop nano vi vim nitrogen autorandr tlp
+sudo pacman -S i3status htop nano vi vim nitrogen autorandr tlp thunar
 
 echo "finished running"
